fcd3817577923c88f1b740674595c2ef1031e262
ab250579 <adrienbessy@hotmail.com>
2016-09-22 13:12:12 +0200

    filtering on one file

---
+++ server.R
@@ -0,0 +1,45 @@
+shinyServer(function(input, output) {
+  datasetInput <- reactive({
+    inFile2 <- paste("data/", input$dataset, sep = "", ".csv") 
+    read.csv(inFile2, header = TRUE, sep = ";", quote = '"')
+  })
+  
+  output$table <- DT::renderDataTable(DT::datatable({
+    data<-datasetInput()
+    if (input$panel1 != "All") {
+      data <- data[data$Functional.category == input$panel1,]
+    }
+    if (input$panel2 != "All") {
+      data <- data[data$D.M == input$panel2,]
+    }
+    if (input$panel3 != "All") {
+      data <- data[data$DC.C == input$panel3,]
+    }
+    data
+  }))
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { paste(input$dataset, '.csv', sep='') },
+    content = function(file) {
+      write.csv(datasetInput(), file)
+    }
+  )
+
+  
+  output$column4_head <- renderText({
+    names(datasetInput())[4]
+  })
+  
+  output$column5_head <- renderText({
+    names(datasetInput())[5]
+  })
+  
+  output$column6_head <- renderText({
+    names(datasetInput())[6]
+  })
+  
+  output$columns <- renderText({
+    names(datasetInput())
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,33 @@
+shinyUI(pageWithSidebar(
+  headerPanel('Download Example'),
+
+  sidebarPanel(
+    selectInput("dataset", "Choose a dataset:", 
+                choices = c("william_2003", "Winter_2014_Winter_seedling")),
+    downloadButton('downloadData', 'Download')
+  ),
+  mainPanel(
+    fluidRow(
+      column(4,
+             selectInput("panel1",
+                         textOutput("column4_head"),
+                         c("All",
+                           "Signaling","Transcription","Protein modification","Sugar/amino acid biosynthesis"	 ))
+      ),
+      column(4,
+             selectInput("panel2",
+                         textOutput("column5_head"),
+                         c("All",
+                           4,3,2.5 ))
+      ),
+      column(4,
+             selectInput("panel3",
+                         textOutput("column6_head"),
+                         c("All",
+                         2,2.5,3))
+      )
+    ),
+    DT::dataTableOutput("table")
+  )
+  
+))
