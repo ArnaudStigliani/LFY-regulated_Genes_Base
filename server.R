e1e97a43818c29648a906b620efe864f33a0a8db
ab250579 <adrienbessy@hotmail.com>
2016-10-11 11:39:44 +0200

    dynamic wigdet generation

--- server.R
+++ server.R
@@ -8,38 +8,53 @@ shinyServer(function(input, output) {
   })
   
   ####################################
+  #### select integer columns
+  integCol <- reactive({
+    nums <- sapply(datasetInput(), is.numeric)
+    datasetInput()[ , nums]
+  })
+  
+  ####################################
+  #### Create widgets in function to column number that contains integer
+  output$numInput <- renderUI({
+    colNum <- ncol(integCol())
+    lapply(1:colNum, function(i) {
+      numericInput(paste0("mini_",colnames(integCol())[i]), paste("min_",names(integCol()[i])), min(integCol()[,i],na.rm=TRUE))
+    })
+  })
+  
+  output$numInput2 <- renderUI({
+    colNum <- ncol(integCol())
+    lapply(1:colNum, function(i) {
+      numericInput(paste0("maxi_",colnames(integCol())[i]), paste("max_",names(integCol()[i])), max(integCol()[,i],na.rm=TRUE))
+    })
+  })
+  
+  # output$boxInput <- renderUI({
+  #   colNum <- ncol(integCol())
+  #   lapply(1:colNum, function(i) {
+  #     checkboxInput(paste0("mini_",colnames(integCol())[i]), paste0("min_",colnames(integCol())[i]))
+  #   })
+  # })
+  
+  # output$boxInput <- renderUI({
+  #   colNum <- ncol(integCol())
+  #   lapply(1:colNum, function(i) {
+  #   checkboxGroupInput(paste0("mini_",colnames(integCol())[i]), paste0("mini_",colnames(integCol())[i]),paste0("mini_",colnames(integCol())[i]))
+  # })
+  # })
+  
+  ####################################
   #### when the user does multiple filters on the table
   datasetInputModif <- reactive({
+    
     data<-datasetInput()
     
-    data <- data[is.na(data$FC_35sLFY_LER) | data$FC_35sLFY_LER >= input$minimum_FC_35LFY_Ler ,]
-    data <- data[is.na(data$FC_35sLFY_LER) | data$FC_35sLFY_LER <= input$maximum_FC_35LFY_Ler ,]
-    
-    data <- data[is.na(data$P.value_35sLFY_LER_down) | data$P.value_35sLFY_LER_down >= input$minimum_P.value_35sLFY_LER_down,]
-    data <- data[is.na(data$P.value_35sLFY_LER_down) | data$P.value_35sLFY_LER_down <= input$maximum_P.value_35sLFY_LER_down,]
-    
-    data <- data[is.na(data$P.value_35sLFY_LER_up) | data$P.value_35sLFY_LER_up >= input$minimum_P.value_35sLFY_LER_up,]
-    data <- data[is.na(data$P.value_35sLFY_LER_up) | data$P.value_35sLFY_LER_up <= input$maximum_P.value_35sLFY_LER_up,]
-    
-    data <- data[is.na(data$FC_Dex_mock) | data$FC_Dex_mock>= input$minimum_FC_DEX_MOCK,]
-    data <- data[is.na(data$FC_Dex_mock) | data$FC_Dex_mock <= input$maximum_FC_DEX_MOCK,]
-    
-    data <- data[is.na(data$P.value_Dex_mock_down) | data$P.value_Dex_mock_down>= input$minimum_P.value_Dex_mock_down,]
-    data <- data[is.na(data$P.value_Dex_mock_down) | data$P.value_Dex_mock_down <= input$maximum_P.value_Dex_mock_down,]
-    
-    data <- data[is.na(data$FC_DexCyclo_cyclo) | data$FC_DexCyclo_cyclo >= input$minimum_FC_DexCyclo_Cyclo,]
-    data <- data[is.na(data$FC_DexCyclo_cyclo) | data$FC_DexCyclo_cyclo <= input$maximum_FC_DexCyclo_Cyclo,]
-    
-    data <- data[is.na(data$FC_LFYGR_WT_GSE28062) | data$FC_LFYGR_WT_GSE28062 >= input$minimum_FC_LFYGR_WT,]
-    data <- data[is.na(data$FC_LFYGR_WT_GSE28062) | data$FC_LFYGR_WT_GSE28062 <= input$maximum_FC_LFYGR_WT,]
-    
-    data <- data[is.na(data$FC_SP1_col) | data$FC_SP1_col >= input$minimum_FC_SP1_col,]
-    data <- data[is.na(data$FC_SP1_col) | data$FC_SP1_col <= input$maximum_FC_SP1_col,]
-    
-    data <- data[is.na(data$FC_SP3_col) | data$FC_SP3_col >= input$minimum_FC_SP3_col,]
-    data <- data[is.na(data$FC_SP3_col) | data$FC_SP3_col <= input$maximum_FC_SP3_col,]
-    
-   
+    for(i in colnames(integCol())){
+    data <- data[is.na(data[[i]]) | data[[i]] >= input[[paste0("mini_",i)]] ,]
+    data <- data[is.na(data[[i]]) | data[[i]] <= input[[paste0("maxi_",i)]] ,]
+    }
+    data
     
   })
   
@@ -74,72 +89,72 @@ shinyServer(function(input, output) {
   
   ####################################
   #### create numeric input to enter min and max
-  output$min_FC_35LFY_Ler<- renderUI({
-    numericInput("minimum_FC_35LFY_Ler", "Min_FC_35LFY_Ler", min(datasetInput()$FC_35sLFY_LER,na.rm=TRUE))
-  })
+  # output$min_FC_35LFY_Ler<- renderUI({
+  #   numericInput("minimum_FC_35LFY_Ler", "Min_FC_35LFY_Ler", min(datasetInput()$FC_35sLFY_LER,na.rm=TRUE))
+  # })
   output$max_FC_35LFY_Ler<- renderUI({
     numericInput("maximum_FC_35LFY_Ler", "Max_FC_35LFY_Ler", max(datasetInput()$FC_35sLFY_LER,na.rm=TRUE))
   })
   
-  output$min_P.value_35sLFY_LER_up<- renderUI({
-    numericInput("minimum_P.value_35sLFY_LER_up", "Min_Pvalue_35sLFY_LER_up", min(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
-  })
+  # output$min_P.value_35sLFY_LER_up<- renderUI({
+  #   numericInput("minimum_P.value_35sLFY_LER_up", "Min_Pvalue_35sLFY_LER_up", min(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
+  # })
   output$max_P.value_35sLFY_LER_up<- renderUI({
     numericInput("maximum_P.value_35sLFY_LER_up", "Max_Pvalue_35sLFY_LER_up", max(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
   })
   
-  output$min_FC_DEX_MOCK<- renderUI({
-    numericInput("minimum_FC_DEX_MOCK", "Min_FC_DEX_MOCK", min(datasetInput()$FC_Dex_mock,na.rm=TRUE))
-  })
+  # output$min_FC_DEX_MOCK<- renderUI({
+  #   numericInput("minimum_FC_DEX_MOCK", "Min_FC_DEX_MOCK", min(datasetInput()$FC_Dex_mock,na.rm=TRUE))
+  # })
   output$max_FC_DEX_MOCK<- renderUI({
     numericInput("maximum_FC_DEX_MOCK", "Max_FC_DEX_MOCK", max(datasetInput()$FC_Dex_mock,na.rm=TRUE))
   })
   
-  output$min_P.value_Dex_mock_down<- renderUI({
-    numericInput("minimum_P.value_Dex_mock_down", "Min_P.value_Dex_mock_down", min(datasetInput()$P.value_Dex_mock_down,na.rm=TRUE))
-  })
+  # output$min_P.value_Dex_mock_down<- renderUI({
+  #   numericInput("minimum_P.value_Dex_mock_down", "Min_P.value_Dex_mock_down", min(datasetInput()$P.value_Dex_mock_down,na.rm=TRUE))
+  # })
   output$max_P.value_Dex_mock_down<- renderUI({
     numericInput("maximum_P.value_Dex_mock_down", "Max_P.value_Dex_mock_down", max(datasetInput()$P.value_Dex_mock_down,na.rm=TRUE))
   })
   
-  output$min_FC_DexCyclo_Cyclo<- renderUI({
-    numericInput("minimum_FC_DexCyclo_Cyclo", "Min_FC_DexCyclo_Cyclo", min(datasetInput()$FC_DexCyclo_cyclo,na.rm=TRUE))
-  })
+  # output$min_FC_DexCyclo_Cyclo<- renderUI({
+  #   numericInput("minimum_FC_DexCyclo_Cyclo", "Min_FC_DexCyclo_Cyclo", min(datasetInput()$FC_DexCyclo_cyclo,na.rm=TRUE))
+  # })
   output$max_FC_DexCyclo_Cyclo<- renderUI({
     numericInput("maximum_FC_DexCyclo_Cyclo", "Max_FC_DexCyclo_Cyclo", max(datasetInput()$FC_DexCyclo_cyclo,na.rm=TRUE))
   })
   
-  output$min_FC_LFYGR_WT<- renderUI({
-    numericInput("minimum_FC_LFYGR_WT", "Min_FC_LFYGR_WT", min(datasetInput()$FC_LFYGR_WT_GSE28062,na.rm=TRUE))
-  })
+  # output$min_FC_LFYGR_WT<- renderUI({
+  #   numericInput("minimum_FC_LFYGR_WT", "Min_FC_LFYGR_WT", min(datasetInput()$FC_LFYGR_WT_GSE28062,na.rm=TRUE))
+  # })
   output$max_FC_LFYGR_WT<- renderUI({
     numericInput("maximum_FC_LFYGR_WT", "Max_FC_LFYGR_WT", max(datasetInput()$FC_LFYGR_WT_GSE28062,na.rm=TRUE))
   })
   
-  output$min_FC_SP1_col<- renderUI({
-    numericInput("minimum_FC_SP1_col", "Min_FC_SP1_col", min(datasetInput()$FC_SP1_col,na.rm=TRUE))
-  })
+  # output$min_FC_SP1_col<- renderUI({
+  #   numericInput("minimum_FC_SP1_col", "Min_FC_SP1_col", min(datasetInput()$FC_SP1_col,na.rm=TRUE))
+  # })
   output$max_FC_SP1_col<- renderUI({
     numericInput("maximum_FC_SP1_col", "Max_FC_SP1_col", max(datasetInput()$FC_SP1_col,na.rm=TRUE))
   })
   
-  output$min_FC_SP3_col<- renderUI({
-    numericInput("minimum_FC_SP3_col", "Min_FC_SP3_col", min(datasetInput()$FC_SP3_col,na.rm=TRUE))
-  })
+  # output$min_FC_SP3_col<- renderUI({
+  #   numericInput("minimum_FC_SP3_col", "Min_FC_SP3_col", min(datasetInput()$FC_SP3_col,na.rm=TRUE))
+  # })
   output$max_FC_SP3_col<- renderUI({
     numericInput("maximum_FC_SP3_col", "Max_FC_SP3_col", max(datasetInput()$FC_SP3_col,na.rm=TRUE))
   })
   
-  output$min_P.value_35sLFY_LER_down<- renderUI({
-    numericInput("minimum_P.value_35sLFY_LER_down", "Min_Pvalue_35LFY_LER_down", min(datasetInput()$P.value_35sLFY_LER_down,na.rm=TRUE))
-  })
+  # output$min_P.value_35sLFY_LER_down<- renderUI({
+  #   numericInput("minimum_P.value_35sLFY_LER_down", "Min_Pvalue_35LFY_LER_down", min(datasetInput()$P.value_35sLFY_LER_down,na.rm=TRUE))
+  # })
   output$max_P.value_35sLFY_LER_down<- renderUI({
     numericInput("maximum_P.value_35sLFY_LER_down", "Max_Pvalue_35LFY_LER_down", max(datasetInput()$P.value_35sLFY_LER_down,na.rm=TRUE))
   })
   
-  output$min_P.value_35sLFY_LER_up<- renderUI({
-    numericInput("minimum_P.value_35sLFY_LER_up", "Min_Pvalue_35sLFY_LER_up", min(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
-  })
+  # output$min_P.value_35sLFY_LER_up<- renderUI({
+  #   numericInput("minimum_P.value_35sLFY_LER_up", "Min_Pvalue_35sLFY_LER_up", min(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
+  # })
   output$max_P.value_35sLFY_LER_up<- renderUI({
     numericInput("maximum_P.value_35sLFY_LER_up", "Max_Pvalue_35sLFY_LER_up", max(datasetInput()$P.value_35sLFY_LER_up,na.rm=TRUE))
   })
--- ui.R
+++ ui.R
@@ -6,39 +6,42 @@ shinyUI(fluidPage(
   sidebarPanel(
     selectInput("dataset", "Choose a dataset:", 
                 fileNames ),
-    checkboxInput("min_FC_35LFY_Ler", "min_FC_35LFY_Ler"),
-    checkboxInput("max_FC_35LFY_Ler", "max_FC_35LFY_Ler"),
+    # ,uiOutput("boxInput"),
+    # checkboxInput("min_FC_35LFY_Ler", "min_FC_35LFY_Ler"),
+    # checkboxInput("max_FC_35LFY_Ler", "max_FC_35LFY_Ler"),
     downloadButton('downloadData', 'Download')
   ),
   
   mainPanel(
     fluidRow(
-      conditionalPanel(
-        condition = "input.min_FC_35LFY_Ler == true",
-        column(2, uiOutput("min_FC_35LFY_Ler") )
-      )
-      ,
-      conditionalPanel(
-        condition = "input.max_FC_35LFY_Ler == true",
-        column(2, uiOutput("max_FC_35LFY_Ler") )
-      )
-      ,
-      column(2, uiOutput("min_P.value_35sLFY_LER_down") ),
-      column(2, uiOutput("max_P.value_35sLFY_LER_down") ),
-      column(2, uiOutput("min_P.value_35sLFY_LER_up") ),
-      column(2, uiOutput("max_P.value_35sLFY_LER_up") ),
-      column(2, uiOutput("min_FC_DEX_MOCK") ),
-      column(2, uiOutput("max_FC_DEX_MOCK") ),
-      column(2, uiOutput("min_P.value_Dex_mock_down") ),
-      column(2, uiOutput("max_P.value_Dex_mock_down") ),
-      column(2, uiOutput("min_FC_DexCyclo_Cyclo") ),
-      column(2, uiOutput("max_FC_DexCyclo_Cyclo") ),
-      column(2, uiOutput("min_FC_LFYGR_WT") ),
-      column(2, uiOutput("max_FC_LFYGR_WT") ),
-      column(2, uiOutput("min_FC_SP1_col") ),
-      column(2, uiOutput("max_FC_SP1_col") ),
-      column(2, uiOutput("min_FC_SP3_col") ),
-      column(2, uiOutput("max_FC_SP3_col") )
+      uiOutput("numInput"),
+      uiOutput("numInput2")
+      # ,conditionalPanel(
+      #   condition = "input.min_FC_35LFY_Ler == true",
+      #   column(2, uiOutput("min_FC_35LFY_Ler") )
+      # )
+      # ,
+      # conditionalPanel(
+      #   condition = "input.max_FC_35LFY_Ler == true",
+      #   column(2, uiOutput("max_FC_35LFY_Ler") )
+      # )
+      # ,
+      # column(2, uiOutput("min_P.value_35sLFY_LER_down") ),
+      # column(2, uiOutput("max_P.value_35sLFY_LER_down") ),
+      # column(2, uiOutput("min_P.value_35sLFY_LER_up") ),
+      # column(2, uiOutput("max_P.value_35sLFY_LER_up") ),
+      # column(2, uiOutput("min_FC_DEX_MOCK") ),
+      # column(2, uiOutput("max_FC_DEX_MOCK") ),
+      # column(2, uiOutput("min_P.value_Dex_mock_down") ),
+      # column(2, uiOutput("max_P.value_Dex_mock_down") ),
+      # column(2, uiOutput("min_FC_DexCyclo_Cyclo") ),
+      # column(2, uiOutput("max_FC_DexCyclo_Cyclo") ),
+      # column(2, uiOutput("min_FC_LFYGR_WT") ),
+      # column(2, uiOutput("max_FC_LFYGR_WT") ),
+      # column(2, uiOutput("min_FC_SP1_col") ),
+      # column(2, uiOutput("max_FC_SP1_col") ),
+      # column(2, uiOutput("min_FC_SP3_col") ),
+      # column(2, uiOutput("max_FC_SP3_col") )
     ),
     
     fluidRow(
